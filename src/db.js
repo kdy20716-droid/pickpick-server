82d499ba2c68147eebf94bd66c92ac42d69bf4fa
Chamisae812 <anjihoon7777@gmail.com>
2026-05-07 19:10:45 +0900

    클라우드 연결

--- src/db.js
+++ src/db.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  ssl: { rejectUnauthorized: true },
 });
 
 export default pool;
